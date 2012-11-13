6557becd6e52624de9840540ec3717f175050da0
Francesco Canessa <makevoid@gmail.com>
2012-11-14 02:26:16 +0900

    supergallery

--- coffee/app.coffee
+++ coffee/app.coffee
@@ -3,7 +3,48 @@ resize_gallery = ->
   $(".gallery").height height
 
 
+# gallery
+
+gal = {
+  photos: []
+}
+window.gal = gal
+
+gal.photos_imgs = ->
+  $(".gallery img")
+
+gal.assign_classes = ->
+  $(gal.photos[0]).addClass "pos0"
+  $(gal.photos[1]).addClass "pos1"
+  $(gal.photos[2]).addClass "pos2"
+  $(gal.photos[3]).addClass "pos3"
+  $(gal.photos[4]).addClass "pos4"
+  $(gal.photos[5]).addClass "pos5"
+
+
+gal.frame = ->
+  gal.photos_imgs().each (id, el) ->
+    $(el).removeClass()
+  console.log gal.photos_imgs()
+  element = gal.photos.shift()
+  gal.photos.push(element)
+  gal.assign_classes()
+
+gal.animate_frame = ->
+  setTimeout ->
+    gal.frame()
+    gal.animate_frame()
+  , 5000
+
+gal.animate = ->
+  gal.frame()
+  gal.animate_frame()
+
+
 $ ->
+  gal.photos = $.makeArray gal.photos_imgs()
+  gal.animate()
+
   resize_gallery()
 
   $(window).resize ->
--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,5 @@
 (function() {
-  var resize_gallery;
+  var gal, resize_gallery;
 
   resize_gallery = function() {
     var height;
@@ -7,7 +7,51 @@
     return $(".gallery").height(height);
   };
 
+  gal = {
+    photos: []
+  };
+
+  window.gal = gal;
+
+  gal.photos_imgs = function() {
+    return $(".gallery img");
+  };
+
+  gal.assign_classes = function() {
+    $(gal.photos[0]).addClass("pos0");
+    $(gal.photos[1]).addClass("pos1");
+    $(gal.photos[2]).addClass("pos2");
+    $(gal.photos[3]).addClass("pos3");
+    $(gal.photos[4]).addClass("pos4");
+    return $(gal.photos[5]).addClass("pos5");
+  };
+
+  gal.frame = function() {
+    var element;
+    gal.photos_imgs().each(function(id, el) {
+      return $(el).removeClass();
+    });
+    console.log(gal.photos_imgs());
+    element = gal.photos.shift();
+    gal.photos.push(element);
+    return gal.assign_classes();
+  };
+
+  gal.animate_frame = function() {
+    return setTimeout(function() {
+      gal.frame();
+      return gal.animate_frame();
+    }, 5000);
+  };
+
+  gal.animate = function() {
+    gal.frame();
+    return gal.animate_frame();
+  };
+
   $(function() {
+    gal.photos = $.makeArray(gal.photos_imgs());
+    gal.animate();
     resize_gallery();
     return $(window).resize(function() {
       return resize_gallery();
