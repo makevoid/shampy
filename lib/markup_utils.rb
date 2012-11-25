76c0095db4ca3a2cd2942f312c8da64c825a1933
fbx10 <cabrini1@hotmail.it>
2012-11-25 17:09:02 +0100

    aggiunto testo lezioni

--- lib/markup_utils.rb
+++ lib/markup_utils.rb
@@ -2,7 +2,7 @@ module MarkupUtils
   def markup(file)
     text = File.read "#{PATH}/texts/#{file}.txtl"
     content = RedCloth.new(text).to_html
-    haml_tag :div, { class: file } do
+    haml_tag :div, { class: "textile #{file}" } do
       haml_concat content
     end
   end
